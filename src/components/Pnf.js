f66cc5ca65bef62a229797115e8c684f8484d008
Dinesh <petadineshkumar97@gmail.com>
2020-12-08 23:50:57 +0530

    commit

---
+++ src/components/Pnf.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Pnf.css';
+
+ function Pnf() {
+    return (
+        <div className="jumbotron">
+           <h1 className="text-center">Page Not Found</h1> 
+        </div>
+    )
+}
+export default Pnf;
